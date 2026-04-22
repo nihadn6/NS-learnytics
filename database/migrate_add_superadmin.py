a90ad968c5a574f38c4019d217d1882336700988
Nihad <76474167+nihadn6@users.noreply.github.com>
2026-04-22 22:26:52 +0530

    Add DB_PORT environment variable for database connection

--- database/migrate_add_superadmin.py
+++ database/migrate_add_superadmin.py
@@ -5,6 +5,7 @@ DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_USER = os.environ.get('DB_USER', 'root')
 DB_PASS = os.environ.get('DB_PASS', '')
 DB_NAME = os.environ.get('DB_NAME', 'ns_learnytics')
+DB_PORT = int(os.environ.get('DB_PORT', 3306))
 
 def get_conn():
     return pymysql.connect(
