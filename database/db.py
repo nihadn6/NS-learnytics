2fc3535913ec0f4d9dd52a7596a56833a8e7164e
Nihad <76474167+nihadn6@users.noreply.github.com>
2026-04-22 22:19:17 +0530

    Make database configuration environment-dependent

--- database/db.py
+++ database/db.py
@@ -4,7 +4,8 @@ import os
 DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_USER = os.environ.get('DB_USER', 'root')
 DB_PASS = os.environ.get('DB_PASS', '')
-DB_NAME = 'ns_learnytics'
+DB_NAME = os.environ.get('DB_NAME', 'ns_learnytics')
+DB_PORT = int(os.environ.get('DB_PORT', 3306))
 
 def get_db_connection():
     # Use utf8 (not utf8mb4) for compatibility with older MySQL versions
@@ -13,6 +14,7 @@ def get_db_connection():
         user=DB_USER,
         password=DB_PASS,
         database=DB_NAME,
+        port = DB_PORT,
         cursorclass=pymysql.cursors.DictCursor,
         charset='utf8',
         init_command='SET NAMES utf8'
